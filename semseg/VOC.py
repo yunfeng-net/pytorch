234970777a3ef76b13f3f18a87c1960516b6adfd
yunfeng-net <yunfeng.net@hotmail.com>
2019-03-31 09:10:25 +0800

    data augmentation

--- semseg/VOC.py
+++ semseg/VOC.py
@@ -6,6 +6,7 @@ import numpy as np
 import PIL.Image as Image
 import scipy.io
 import torch
+import torchvision
 from torch.utils import data
 from matplotlib import pyplot as plt
 import random
@@ -42,7 +43,18 @@ index2name = ['background', 'airplane', 'bicycle', 'bird', 'boat', 'bottle', 'bu
               'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person', 'potted plant',
               'sheep', 'sofa', 'train', 'tv/monitor']
 
-def crop_image(image, lbl = 0): 
+def crop_image(image, flag, lbl = 0): 
+
+    h_top = flag[0]*32
+    h_bottom = h_top + 160
+    w_left = flag[1]*32
+    w_right = w_left + 160
+    if lbl==0:
+        return image[h_top:h_bottom, w_left:w_right, :] 
+    else:
+        return image[h_top:h_bottom, w_left:w_right] 
+
+def crop_image2(image, lbl = 0): 
     h = ((image.shape[0] >>5)<<5)
     w = ((image.shape[1] >>5)<<5)
     #h,w = 96,96
@@ -54,7 +66,6 @@ def crop_image(image, lbl = 0):
         return image[h_top:h_bottom, w_left:w_right, :] 
     else:
         return image[h_top:h_bottom, w_left:w_right] 
-
 
 
 class VOCClassSegBase(data.Dataset):
@@ -83,12 +94,11 @@ class VOCClassSegBase(data.Dataset):
         'tv/monitor',
     ])
     mean_bgr = np.array([104.00698793, 116.66876762, 122.67891434])
-    def __init__(self, root='/home/yunfeng/data/VOCdevkit/VOC2012', phase='train', transform=False):
+    def __init__(self, root='/home/yunfeng/data/VOCdevkit/VOC2012', phase='train'):
         self.root = root
-        self._transform = transform
         self.flip_rate = 0
-        #if phase == 'train':
-        #        self.flip_rate = 0.5
+        if phase == 'train':
+            self.flip_rate = 0.5
 
         # VOC2011 and others are subset of VOC2012
         dataset_dir = root
@@ -112,35 +122,32 @@ class VOCClassSegBase(data.Dataset):
         data_file = self.files[index]
         # load image
         img_file = data_file['img']
-        #print(img_file)
-        img =  cv2.imread(img_file)
-        img = cv2.resize(img, (160, 160))
+        img =  Image.open(img_file)
+        img = img.resize((160, 160))
         #img = np.array(img, dtype=np.uint8)
-        #rd = random.random() 
-        #if rd < self.flip_rate:
-        #    img   = np.fliplr(img)
-        #assert img.shape[0]>96
-        #img = crop_image(img)
-        #assert img.shape[0]==96
         # load label
         label_file = data_file['label']
         #label = cv2.imread(label_file)
         label = Image.open(label_file)
-        label =label.resize((160,160))
+        label = label.resize((160,160))
+        rd = random.random() 
+        if rd < self.flip_rate:
+            img   = img.transpose(Image.FLIP_LEFT_RIGHT)
+            label = label.transpose(Image.FLIP_LEFT_RIGHT)
+        if self.flip_rate>0:
+            p = torchvision.transforms.Pad(32)
+            img = p(img)
+            label = p(label)
+        h = np.random.randint(3, size=2)
+        img = np.array(img, dtype=np.uint8)
         label = np.array(label, dtype=np.int32)
-        #if rd< self.flip_rate:
-        #   label = np.fliplr(label)
-        #label = crop_image(label,1)
-        #lbl = -1*np.ones((label.shape[0], label.shape[1]), dtype=np.int64)
-        #for i, color in enumerate(index2color):
-        #    lbl[np.where(np.all(label == color, axis=2))] = i
+        if self.flip_rate>0:
+            img = crop_image(img,h)
+            label = crop_image(label,h,1)
         label[label >20] = 0
         lbl = label
-        #if self._transform:
         img,lbl = self.transform(img, lbl)
         assert lbl.max()<21
-        #print(index,label.size(),img.size())
-        #assert lbl.size()[0]==img.size()[1] and lbl.size()[1]==img.size()[2] 
         return img, lbl
 
     def transform(self, img, label):
@@ -161,7 +168,7 @@ class VOCClassSegBase(data.Dataset):
         label = label.numpy()
         return img, label
 num_class = 21
-batch_size = 5
+batch_size = 1
 train_data = VOCClassSegBase(phase='train')
 train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=8)
 test_data = VOCClassSegBase(phase='val')
@@ -170,6 +177,7 @@ if __name__ == "__main__":
     sb = VOCClassSegBase(phase='train')
     for i in range(len(sb)):
         sample,label = sb[i]
+        #print(sample.size(), label.size())
         #assert sample['X'].size()[1]==96
         #print(i, sample['X'].size(), sample['Y'].size())
         #plt.figure()
